f0cceb1aed4df55417e1dc0839341027e6d038f3
piotrsobczak.wro <piotrsobczak.wro@hotmail.com>
2022-08-12 15:38:16 +0200

    Changes in file: git-upgrade-readme.bash

--- git/git-upgrade-readme.bash
+++ git/git-upgrade-readme.bash
@@ -2,7 +2,9 @@
 
 files=$(git ls-files)
 
+echo "" > ../README.md
+
 for files_to_readme in ${files}
 do 
-  echo "[${files_to_readme}](https://github.com/piotrsobczakwro/bash/tree/main/${files_to_readme})" > ../README.md
+  echo "[${files_to_readme}](https://github.com/piotrsobczakwro/bash/blob/main/git/${files_to_readme})  " >> ../README.md
 done
