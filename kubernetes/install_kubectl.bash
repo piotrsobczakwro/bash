9e7260c50c3bde60fbf42acd68361b240a014c39
piotrsobczak.wro <piotrsobczak.wro@hotmail.com>
2022-09-01 11:40:55 +0200

    Changes in file: ../kubernetes/

---
+++ kubernetes/install_kubectl.bash
@@ -0,0 +1,13 @@
+    KUBECTL_BIN=kubectl
+     
+    function install_kubectl {
+        if [ -z $(which $KUBECTL_BIN) ]
+           then
+               curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/$KUBECTL_BIN
+               chmod +x ${KUBECTL_BIN}
+               sudo mv ${KUBECTL_BIN} /usr/local/bin/${KUBECTL_BIN}
+        else
+		echo "Kubectl is already installed"
+        fi
+     
+    }
